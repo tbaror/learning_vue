553d29822b0543e54ce889884f10d42473df68b1
tbaror <tal_baror@hotmail.com>
2022-09-11 23:07:40 +0300

    section 2 24 event continue

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
+  methods : {
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+        },
+    reduce(num) {
+      this.counter = this.counter - num;
+    }    
+  }
 });
 
 app.mount('#events');
