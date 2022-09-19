d099d018586e9ae8148fbe2a86c633cf3a4c5032
tbaror <tal_baror@hotmail.com>
2022-09-19 20:52:03 +0300

    section 2 28

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    outputFullname
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
